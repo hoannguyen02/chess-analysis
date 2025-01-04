4767fc40ad6d64a0108e34f6cd1b5ee08fc42beb
hoannguyen02 <nguyenvanhoan02@gmail.com>
2025-01-04 13:41:56 +0700

    Basic layout create puzzle page

--- src/screens/puzzles/create/create-puzzle-screen.tsx
+++ src/screens/puzzles/create/create-puzzle-screen.tsx
@@ -1,11 +1,16 @@
 import { TitlePage } from '@/components/TitlePage';
-import { Button, Label, TextInput } from 'flowbite-react';
+import { PuzzleDifficulty, PuzzlePhase, PuzzleStatus } from '@/types/puzzle';
+import { Button, Checkbox, Label, Select, TextInput } from 'flowbite-react';
 import Link from 'next/link';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { VscEdit } from 'react-icons/vsc';
 
 type CreatePuzzleForm = {
   fen: string;
+  status: PuzzleStatus;
+  difficulty: PuzzleDifficulty;
+  isPublic: boolean;
+  phase?: PuzzlePhase;
 };
 
 export const CreatePuzzleScreen = () => {
@@ -15,7 +20,13 @@ export const CreatePuzzleScreen = () => {
     formState: { errors }, // Access form errors
     watch,
   } = useForm<CreatePuzzleForm>({
-    defaultValues: { fen: '8/8/8/8/8/8/8/8 w - - 0 1' },
+    defaultValues: {
+      fen: '8/8/8/8/8/8/8/8 w - - 0 1',
+      isPublic: false,
+      status: 'Draft',
+      difficulty: 'Easy',
+      phase: 'Middle',
+    },
   });
 
   // Handle form submission
@@ -26,32 +37,73 @@ export const CreatePuzzleScreen = () => {
   const fen = watch('fen');
 
   return (
-    <div className="grid grid-cols-2">
+    <div className="">
       <TitlePage>Create Puzzle</TitlePage>
       <form onSubmit={handleSubmit(onSubmit)} className="">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-3  place-content-start mb-4 gap-8">
+          <div className="flex flex-col">
+            <div className="w-[90%]">
+              <Label htmlFor="fen" value="Fen* " />
+              <TextInput
+                id="fen"
+                type="text"
+                placeholder="FEN string"
+                {...register('fen', { required: 'FEN is required' })}
+                color={errors.fen ? 'failure' : undefined}
+              />
+              {errors.fen && (
+                <p className="text-red-500 text-sm">{errors.fen.message}</p>
+              )}
+            </div>
+            <div className="mt-2">
+              <Link
+                href={`/fen-builder?fen=${fen}`}
+                className="w-full flex items-center text-[12px]"
+                target="_blank"
+              >
+                Update FEN: <VscEdit className="ml-2" />
+              </Link>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="mr-4">
+              <Label htmlFor="status" value="Status" />
+            </div>
+            <Select id="status" required {...register('status')}>
+              <option>Draft</option>
+              <option>Verified</option>
+            </Select>
+          </div>
+          <div className="flex items-center">
+            <div className="mr-4">
+              <Label htmlFor="isPublic" value="Public (All users can see it)" />
+            </div>
+            <Checkbox id="isPublic" {...register('isPublic')} />
+          </div>
+        </div>
+        <div className="grid grid-cols-3  place-content-start mb-4 gap-8">
           <div>
-            <Label htmlFor="fen" value="Fen" />
-            <TextInput
-              id="fen"
-              type="text"
-              placeholder="FEN string"
-              {...register('fen', { required: 'FEN is required' })}
-              color={errors.fen ? 'failure' : undefined}
-            />
-            {errors.fen && (
-              <p className="text-red-500 text-sm">{errors.fen.message}</p>
-            )}
+            <div className="mb-2">
+              <Label htmlFor="difficulty" value="Difficulty" />
+            </div>
+            <Select id="difficulty" required {...register('difficulty')}>
+              <option label="Easy: 800-1200">Easy</option>
+              <option label="Medium: 1200-1600">Medium</option>
+              <option label="Hard: 1600-2000">Hard</option>
+              <option label="Very hard: 2000+">Very hard</option>
+            </Select>
           </div>
           <div>
-            <Link
-              href={`/fen-builder?fen=${fen}`}
-              className="w-full"
-              target="_blank"
-            >
-              <VscEdit />
-            </Link>
+            <div className="mb-2">
+              <Label htmlFor="phase" value="Phase" />
+            </div>
+            <Select value={watch('phase')} id="phase" {...register('phase')}>
+              <option>Opening</option>
+              <option>Middle</option>
+              <option>Endgame</option>
+            </Select>
           </div>
+          <div></div>
         </div>
 
         <Button type="submit" gradientMonochrome="info">
---
+++ src/types/puzzle.ts
@@ -0,0 +1,9 @@
+export type PuzzleStatus = 'Draft' | 'Verified';
+/**
+ * 800-1200: Easy
+ * 1200-1600: Medium
+ * 1600-2000: Hard
+ * 2000+: Very hard
+ */
+export type PuzzleDifficulty = 'Easy' | 'Medium' | 'Hard' | 'Very Hard';
+export type PuzzlePhase = 'Opening' | 'Middle' | 'Endgame';
