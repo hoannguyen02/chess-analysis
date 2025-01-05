8e6c1eae35b153f6d92ea656572d411ea669d7b9
hoannguyen02 <nguyenvanhoan02@gmail.com>
2025-01-05 16:30:37 +0700

    basic query string

--- src/screens/puzzles/puzzle-list.tsx
+++ src/screens/puzzles/puzzle-list.tsx
@@ -1,10 +1,26 @@
 import useSWR from 'swr';
 import { fetcher } from './fetcher';
 import { useAppContext } from '@/contexts/AppContext';
+import { useMemo, useState } from 'react';
+import { PuzzleDifficulty, PuzzlePhase, PuzzleStatus } from '@/types/puzzle';
+import { PuzzleTheme } from '@/types/puzzle-theme';
 
 export const PuzzleListScreen = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [phase, setPhase] = useState<PuzzlePhase | undefined>();
+  const [status, setStatus] = useState<PuzzleStatus | undefined>();
+  const [theme, setTheme] = useState<PuzzleTheme | undefined>();
+  const [difficulty, setDifficulty] = useState<PuzzleDifficulty | undefined>();
+
+  const queryString = useMemo(() => {
+    return `phase=${phase}&theme=${theme}&difficulty=${difficulty}&status=${status}&page=${currentPage}`;
+  }, [phase, theme, difficulty, status, currentPage]);
+
   const { apiDomain } = useAppContext();
-  const { data, error, isLoading } = useSWR(`${apiDomain}/v1/puzzles`, fetcher);
+  const { data, error, isLoading } = useSWR(
+    `${apiDomain}/v1/puzzles?${queryString}`,
+    fetcher
+  );
   console.log('data', data);
 
   if (isLoading) return <div>Loading...</div>;
