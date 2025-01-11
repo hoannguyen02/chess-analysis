62fe922c1933169ded0613af54d75934a7a19848
hoannguyen02 <nguyenvanhoan02@gmail.com>
2025-01-11 21:58:12 +0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
   const t = useTranslations('common');
   return (
     <footer className="py-[1rem] flex justify-center flex-col">
-      <div className="grid lg:grid-cols-2 lg:w-[600px] lg:mx-auto mb-4 lg:mt-8 lg:gap-4 gap-2">
+      <div className="grid lg:grid-cols-2 lg:w-[60%] lg:mx-auto mb-4 lg:mt-8 lg:gap-4 gap-2">
         <ul className="flex justify-around">
           <li className="p-0">
             <Link className="hover:text-[var(--s-bg)]" href="/privacy-policy">
