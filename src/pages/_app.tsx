aaaa43cfca6a17f7dea48acc640b9e6de5f3b830
hoannguyen02 <nguyenvanhoan02@gmail.com>
2025-02-18 19:02:28 +0700

    only apply for local tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -63,15 +63,18 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       {/* Google Tag Manager */}
-      <Script id="gtm-script" strategy="afterInteractive">
-        {`
+      {process.env.NODE_ENV === 'production' && (
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-TFV27HSJ');
         `}
-      </Script>
+        </Script>
+      )}
+
       <ErrorBoundary
         msg={locale === 'vi' ? 'Đã xảy ra lỗi!' : 'Something went wrong!'}
       >
