39f4f70496c8c595c9b74d417870eae1b8ef465e
hoannguyen02 <nguyenvanhoan02@gmail.com>
2025-05-14 08:44:37 +0700

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,4 @@
-import { useRouter } from 'next/router';
-import React, { ReactNode, useMemo } from 'react';
-import { Footer } from './Footer';
+import React, { ReactNode } from 'react';
 import Header from './Header';
 
 interface LayoutProps {
@@ -8,12 +6,6 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const router = useRouter();
-
-  const isLessonPath = useMemo(() => {
-    return router.asPath.includes('/lessons');
-  }, [router.asPath]);
-
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }} className="">
       <div className="flex flex-col min-h-screen">
@@ -21,7 +13,6 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
         <main className="mx-auto max-w-[1172px] w-full py-4 min-h-[85vh] flex-grow px-[1rem] pb-20">
           {children}
         </main>
-        {!isLessonPath && <Footer />}
       </div>
     </div>
   );
